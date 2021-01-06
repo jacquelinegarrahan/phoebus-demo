43a1c13b26d586b3654da19db10ed89d830cb1dd
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-01-06 09:53:22 -0800

    Setup demo

---
+++ serve_pvs.py
@@ -0,0 +1,35 @@
+import argparse
+from pcaspy import Driver, SimpleServer
+from pcaspy.tools import ServerThread
+
+
+class myDriver(Driver):
+    def  __init__(self):
+        super(myDriver, self).__init__()
+
+
+if __name__ == "__main__":
+
+    pvdb = {}
+
+    for i in range(0, 6):
+        print("Serving...")
+        pvname=f"PV{i}"
+        pvdb[pvname] = {
+                        "type": "float",
+                        "value": 0.5,
+                        "hihi": 3.0,
+                        "hi":  2.0,
+                        "lo": 0.5,
+                        "lolo": 0.0
+                    }
+
+    server = SimpleServer()
+    server.createPV("DEMO" + ":", pvdb)
+    driver = myDriver()
+
+    try:
+        while True:
+            server.process(0.1)
+    except KeyboardInterrupt:
+        print("Shutting down server...")
